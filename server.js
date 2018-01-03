1e22c50f8f105732bbb6a30d2786a8437c5cc504
jdcook <jdcook@barracuda.com>
2018-01-03 12:12:57 -0500

    v 1.1.0

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const version = require('./package.json').version;
+const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World! Version: '+ version));
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
